b72dfd1f8b4c028dbc70d18981bf7e1468b268f7
Dan Cojocaru <dancojocaru2010@hotmail.com>
2015-12-26 16:22:38 +0200

    Initialise git First commit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Countdown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.hours_input = new System.Windows.Forms.TextBox();
+            this.hours_label = new System.Windows.Forms.Label();
+            this.seconds_input = new System.Windows.Forms.TextBox();
+            this.seconds_label = new System.Windows.Forms.Label();
+            this.minutes_input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.time_label = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.elapsed = new System.Windows.Forms.Label();
+            this.elapsed_progress = new System.Windows.Forms.ProgressBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the countdown here!";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(188, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start countdown";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // hours_input
+            // 
+            this.hours_input.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.hours_input.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hours_input.Location = new System.Drawing.Point(12, 41);
+            this.hours_input.MaxLength = 9;
+            this.hours_input.Name = "hours_input";
+            this.hours_input.Size = new System.Drawing.Size(63, 22);
+            this.hours_input.TabIndex = 1;
+            // 
+            // hours_label
+            // 
+            this.hours_label.AutoSize = true;
+            this.hours_label.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hours_label.Location = new System.Drawing.Point(13, 64);
+            this.hours_label.Name = "hours_label";
+            this.hours_label.Size = new System.Drawing.Size(37, 13);
+            this.hours_label.TabIndex = 3;
+            this.hours_label.Text = "hours";
+            // 
+            // seconds_input
+            // 
+            this.seconds_input.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.seconds_input.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seconds_input.Location = new System.Drawing.Point(245, 41);
+            this.seconds_input.MaxLength = 2;
+            this.seconds_input.Name = "seconds_input";
+            this.seconds_input.Size = new System.Drawing.Size(63, 22);
+            this.seconds_input.TabIndex = 3;
+            this.seconds_input.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // seconds_label
+            // 
+            this.seconds_label.AutoSize = true;
+            this.seconds_label.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seconds_label.Location = new System.Drawing.Point(259, 64);
+            this.seconds_label.Name = "seconds_label";
+            this.seconds_label.Size = new System.Drawing.Size(49, 13);
+            this.seconds_label.TabIndex = 5;
+            this.seconds_label.Text = "seconds";
+            // 
+            // minutes_input
+            // 
+            this.minutes_input.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutes_input.Location = new System.Drawing.Point(131, 41);
+            this.minutes_input.MaxLength = 2;
+            this.minutes_input.Name = "minutes_input";
+            this.minutes_input.Size = new System.Drawing.Size(63, 22);
+            this.minutes_input.TabIndex = 2;
+            this.minutes_input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.minutes_input.TextChanged += new System.EventHandler(this.minutes_input_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(131, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "minutes";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // time_label
+            // 
+            this.time_label.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.time_label.BackColor = System.Drawing.Color.Transparent;
+            this.time_label.Location = new System.Drawing.Point(13, 129);
+            this.time_label.Name = "time_label";
+            this.time_label.Size = new System.Drawing.Size(134, 13);
+            this.time_label.TabIndex = 0;
+            this.time_label.Visible = false;
+            this.time_label.Click += new System.EventHandler(this.time_label_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 103);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(296, 23);
+            this.progressBar.TabIndex = 0;
+            this.progressBar.Visible = false;
+            // 
+            // elapsed
+            // 
+            this.elapsed.BackColor = System.Drawing.Color.Transparent;
+            this.elapsed.Location = new System.Drawing.Point(163, 129);
+            this.elapsed.Name = "elapsed";
+            this.elapsed.Size = new System.Drawing.Size(145, 13);
+            this.elapsed.TabIndex = 0;
+            this.elapsed.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.elapsed.Visible = false;
+            // 
+            // elapsed_progress
+            // 
+            this.elapsed_progress.Location = new System.Drawing.Point(12, 145);
+            this.elapsed_progress.Name = "elapsed_progress";
+            this.elapsed_progress.Size = new System.Drawing.Size(296, 23);
+            this.elapsed_progress.TabIndex = 0;
+            this.elapsed_progress.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Countdown
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(320, 176);
+            this.Controls.Add(this.elapsed_progress);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.minutes_input);
+            this.Controls.Add(this.seconds_label);
+            this.Controls.Add(this.seconds_input);
+            this.Controls.Add(this.hours_label);
+            this.Controls.Add(this.hours_input);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.time_label);
+            this.Controls.Add(this.elapsed);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Countdown";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Countdown";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox hours_input;
+        private System.Windows.Forms.Label hours_label;
+        private System.Windows.Forms.TextBox seconds_input;
+        private System.Windows.Forms.Label seconds_label;
+        private System.Windows.Forms.TextBox minutes_input;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label time_label;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label elapsed;
+        private System.Windows.Forms.ProgressBar elapsed_progress;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Countdown : Form
+    {
+        public Countdown()
+        {
+            InitializeComponent();
+        }
+
+        int miliseconds, taskbar;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Composing the time in h,m,s
+            if (hours_input.Text == "") hours_input.Text = "00";
+            if (minutes_input.Text == "") minutes_input.Text = "00";
+            if (seconds_input.Text == "") seconds_input.Text = "00";
+            for (int i = 0; i <= 9; i++)
+            {
+                if (Convert.ToInt32(hours_input.Text) == i) hours_input.Text = "0" + i;
+                if (Convert.ToInt32(minutes_input.Text) == i) minutes_input.Text = "0" + i;
+                if (Convert.ToInt32(seconds_input.Text) == i) seconds_input.Text = "0" + i;
+            }
+            int hours, minutes, seconds;
+            hours = Convert.ToInt32(hours_input.Text);
+            minutes = Convert.ToInt32(minutes_input.Text);
+            seconds = Convert.ToInt32(seconds_input.Text);
+
+            //Composing the time in miliseconds
+            miliseconds = (hours * 3600 + minutes * 60 + seconds) * 4 + 1;
+
+            progressBar.Maximum = miliseconds - 1;
+            progressBar.Value = miliseconds - 1;
+            elapsed_progress.Maximum = progressBar.Maximum;
+
+            //Starting countdown
+            timer1.Enabled = true;
+            progressBar.Visible = true;
+            time_label.Visible = true;
+            elapsed_progress.Visible = true;
+            elapsed.Visible = true;
+            taskbar = 0;
+            seconds_input.ReadOnly = true;
+            minutes_input.ReadOnly = true;
+            hours_input.ReadOnly = true;
+            button1.Enabled = false;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            elapsed_progress.Visible = false;
+            elapsed.Visible = false;
+            taskbar = 0;
+            progressBar.Value = 0;
+            elapsed_progress.Value = 0;
+            elapsed.Text = "";
+            this.Text = "Countdown";
+            seconds_input.ReadOnly = false;
+            minutes_input.ReadOnly = false;
+            hours_input.ReadOnly = false;
+            button1.Enabled = true;
+            progressBar.Visible = false;
+
+            hours_input.Text = "";
+            minutes_input.Text = "";
+            seconds_input.Text = "";
+
+            ModifyProgressBarColor.SetState(elapsed_progress, 1);
+            ModifyProgressBarColor.SetState(progressBar, 1);
+
+            TaskbarProgress.SetState(this.Handle, TaskbarProgress.TaskbarStates.NoProgress);
+
+            timer2.Enabled = false;
+        }
+
+        private void minutes_input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void time_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (taskbar > 50) taskbar = 0;
+            if (taskbar < 20 && taskbar >= 0)
+            {
+                this.Text = (miliseconds / 4) + " seconds left!";
+                //TaskbarProgress.SetValue(this.Handle, progressBar.Value, progressBar.Maximum);
+            }
+            if (taskbar >= 20 && taskbar <= 40) 
+            {
+                this.Text = (elapsed_progress.Maximum / 4) - (miliseconds / 4) + " seconds elapsed!";
+                //TaskbarProgress.SetValue(this.Handle, elapsed_progress.Value, elapsed_progress.Maximum);
+            }
+            if (taskbar > 40 && taskbar <= 50)
+            {
+                this.Text = (elapsed_progress.Maximum / 4) + " seconds countdown";
+            }
+            taskbar++;
+            TaskbarProgress.SetValue(this.Handle, progressBar.Value, progressBar.Maximum);
+
+
+            miliseconds--;
+            progressBar.Value = miliseconds;
+            time_label.Text = ( miliseconds / 4 ) + " seconds left!";
+            elapsed_progress.Value = progressBar.Maximum - progressBar.Value;
+            elapsed.Text = (elapsed_progress.Maximum / 4) - (miliseconds / 4) + " seconds elapsed!";
+
+            if ( miliseconds == 0 )
+            {
+                timer1.Enabled = false;
+                time_label.Text = "Countdown expired!";
+                timer2.Enabled = true;
+                this.Text = "Countdown expired!";
+                ModifyProgressBarColor.SetState(elapsed_progress, 2);
+                ModifyProgressBarColor.SetState(progressBar, 2);
+
+                TaskbarProgress.SetValue(this.Handle, progressBar.Maximum, progressBar.Maximum);
+                TaskbarProgress.SetState(this.Handle, TaskbarProgress.TaskbarStates.Error);
+            }
+        }
+    }
+
+
+
+    //Progress bar color
+
+    public static class ModifyProgressBarColor
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr w, IntPtr l);
+        public static void SetState(this ProgressBar pBar, int state)
+        {
+            SendMessage(pBar.Handle, 1040, (IntPtr)state, IntPtr.Zero);
+        }
+    }
+
+    //Taskbar progressbar
+
+    public static class TaskbarProgress
+    {
+        public enum TaskbarStates
+        {
+            NoProgress = 0,
+            Indeterminate = 0x1,
+            Normal = 0x2,
+            Error = 0x4,
+            Paused = 0x8
+        }
+
+        [ComImportAttribute()]
+        [GuidAttribute("ea1afb91-9e28-4b86-90e9-9e9f8a5eefaf")]
+        [InterfaceTypeAttribute(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface ITaskbarList3
+        {
+            // ITaskbarList
+            [PreserveSig]
+            void HrInit();
+            [PreserveSig]
+            void AddTab(IntPtr hwnd);
+            [PreserveSig]
+            void DeleteTab(IntPtr hwnd);
+            [PreserveSig]
+            void ActivateTab(IntPtr hwnd);
+            [PreserveSig]
+            void SetActiveAlt(IntPtr hwnd);
+
+            // ITaskbarList2
+            [PreserveSig]
+            void MarkFullscreenWindow(IntPtr hwnd, [MarshalAs(UnmanagedType.Bool)] bool fFullscreen);
+
+            // ITaskbarList3
+            [PreserveSig]
+            void SetProgressValue(IntPtr hwnd, UInt64 ullCompleted, UInt64 ullTotal);
+            [PreserveSig]
+            void SetProgressState(IntPtr hwnd, TaskbarStates state);
+        }
+
+        [GuidAttribute("56FDF344-FD6D-11d0-958A-006097C9A090")]
+        [ClassInterfaceAttribute(ClassInterfaceType.None)]
+        [ComImportAttribute()]
+        private class TaskbarInstance
+        {
+        }
+
+        private static ITaskbarList3 taskbarInstance = (ITaskbarList3)new TaskbarInstance();
+        private static bool taskbarSupported = Environment.OSVersion.Version >= new Version(6, 1);
+
+        public static void SetState(IntPtr windowHandle, TaskbarStates taskbarState)
+        {
+            if (taskbarSupported) taskbarInstance.SetProgressState(windowHandle, taskbarState);
+        }
+
+        public static void SetValue(IntPtr windowHandle, double progressValue, double progressMax)
+        {
+            if (taskbarSupported) taskbarInstance.SetProgressValue(windowHandle, (ulong)progressValue, (ulong)progressMax);
+        }
+    }
+}
